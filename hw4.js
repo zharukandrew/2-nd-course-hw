0e34b8dcc26cca76a0210140ede81e533b320005
andrew.zharuck@gmail.com <andrew.zharuck@gmail.com>
2023-04-07 19:28:58 +0400

    создал новые ветки и папки

---
+++ hw4.js
@@ -0,0 +1,41 @@
+/*
+ Задание 1
+let i=0;
+while (i<2) {
+    console.log('Привет');
+    i++;
+}*/
+/*
+Задание 2
+for (let i = 1; i <= 5; i++) {
+        console.log(i);
+  }*
+  /*Задание 3
+
+  for (let i = 7; i <= 22; i++) {
+        console.log(i);
+  }*/
+  /*
+  Задание 4
+  let obj = {"Коля": 200, "Вася": 300, "Петя": 400};
+  for (key in obj) {
+       console.log(`${key} – зарплата ${obj[key]}`);
+    }*/
+/*Задание 5
+    let num = 0;
+    for (let n = 1000; n > 50; n++) 
+    {
+        n = (n / 2) ;
+            console.log( n );
+        }
+*/
+
+    /*Задание 6
+    let friday = 0;
+
+ for (let month = 0; friday < 4; friday++) {
+    month += 7;
+    console.log(`Сегодня пятница, ${month}-ое число. Необходимо подготовить отчёт`);
+ }
+    */
+
