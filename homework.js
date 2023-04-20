50f27ccb65fbdf08cc8aa416a5f0ba8233f80496
andrew.zharuck@gmail.com <andrew.zharuck@gmail.com>
2023-04-20 16:04:52 +0400

    добавил еще материалов

--- homework.js
+++ homework.js
@@ -1,5 +1,5 @@
 // 2.5 Функции,8задание
-function seasonsGame() {
+/*function seasonsGame() {
     let season = Number(prompt("Введите номер любого месяца года"));
                                        
     if (season > 0 && season <= 2 || season === 12) {
@@ -13,4 +13,23 @@ function seasonsGame() {
     } else {
       console.log('неверный месяц');
     }
+    }*/
+    // 2.7 Встроенные обьекты
+    let array = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+
+function rememberWords(){
+    alert(array = array.sort(() => Math.random() - 0.5));
+
+    let firstElement = prompt('Чему равнялся первый элемент массива?');
+    let lastElement = prompt('Чему равнялся последний элемент массива?');
+    
+    if (firstElement.toLowerCase() == array[0].toLowerCase() && lastElement.toLowerCase() == array[array.length - 1].toLowerCase()){
+        alert("Поздравляю, вы все угадали!")
+    }else if(firstElement.toLowerCase() == array[0].toLowerCase() || lastElement.toLowerCase() == array[array.length - 1].toLowerCase()){
+        alert("Вы были близки к победе!")
+    }else {
+        alert("Вы ответили неверно!")
     }
+
+}
+
